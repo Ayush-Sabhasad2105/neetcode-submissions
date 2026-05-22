08c112849f8df0e437a3792c303a6e3264603527
Ayush-Sabhasad2105 <ayushsabhasad@gmail.com>
2026-05-22 13:14:28 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+
+        int left = 0;
+        int right = heights.size() - 1;
+        int max_area = 0;
+
+        while(left < right){
+
+            int breadth = right - left;
+            int length = min(heights[left], heights[right]);
+            int area = length * breadth;
+
+            max_area = max(max_area, area);
+
+            if(heights[left] <= heights[right]){
+                left++;
+            }
+
+            else {
+                right--;
+            }
+        }
+
+        return max_area;
+    }
+};
